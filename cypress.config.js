29cd4f753524d7735fa9a345cb01e010c70144b3
Daniela Custodio <DCusto01@its.jnj.com>
2024-09-18 11:39:29 -0300

    Atualizaçao da URL do vendedor online

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://qa-vendedoronline.viavarejo.com.br',
+    baseUrl: 'https://qa-vendedoronline.viavarejo.com.br/cb/#/login',
     viewportWidth: 1072,
     viewportHeight: 893,
     setupNodeEvents(on, config) {
